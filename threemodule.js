57d86d29204720f51a0011a869735f397d65a10d
boouee <104202645+boouee@users.noreply.github.com>
2024-06-04 16:22:49 +0300

    Update threemodule.js

--- threemodule.js
+++ threemodule.js
@@ -56,7 +56,7 @@
 					document.getElementById('button').innerHTML = screen.width / screen.height;
 					if (screen.width / screen.height < 0.8) {
 						//image.style.width = "100%";
-						image.style.minWidth = screen.width;
+						image.style.minWidth = screen.width * 0.9 + "px";
 						console.log(window.innerWidth / window.innerHeight, image.style.minWidth)
 					}
 					//number.textContent = ( i / 5 ) + 1;
